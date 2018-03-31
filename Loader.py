23e8b525be809b3f46202fb9ef4b984680010214
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-03-31 21:09:49 +0300

    generate_dishes append loader, add def create_cook_book

--- Loader.py
+++ Loader.py
@@ -1,28 +1,27 @@
-cook_book = {
-    'яйчница': [
-        {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
-        {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
-    ],
-    'стейк': [
-        {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
-        {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},
-        {'ingridient_name': 'масло', 'quantity': 10, 'measure': 'мл.'}
-    ],
-    'салат': [
-        {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
-        {'ingridient_name': 'огурцы', 'quantity': 100, 'measure': 'гр.'},
-        {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
-        {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
-    ]
-}
+# cook_book = {
+#     'яйчница': [
+#         {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
+#         {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
+#     ],
+#     'стейк': [
+#         {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
+#         {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},
+#         {'ingridient_name': 'масло', 'quantity': 10, 'measure': 'мл.'}
+#     ],
+#     'салат': [
+#         {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
+#         {'ingridient_name': 'огурцы', 'quantity': 100, 'measure': 'гр.'},
+#         {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
+#         {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
+#     ]
+# }
 
 
-def get_shop_list_by_dishes(dishes, person_count):
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
     shop_list = {}
     for dish in dishes:
         for ingridient in cook_book[dish]:
             new_shop_list_item = dict(ingridient)
-
             new_shop_list_item['quantity'] *= person_count
             if new_shop_list_item['ingridient_name'] not in shop_list:
                 shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
@@ -40,8 +39,28 @@ def create_shop_list():
     person_count = int(input('Введите количество человек: '))
     dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
         .lower().split(', ')
-    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    shop_list = get_shop_list_by_dishes(dishes, person_count, create_cook_book())
     print_shop_list(shop_list)
+
+
+def create_cook_book():
+    finish_cook_book = {}
+    with open('Dishes.txt', encoding = 'utf-8') as CookBook:
+        for line_dish in CookBook:
+            ingredients = []
+            amount_ingredients = int(CookBook.readline())
+            for i in range(amount_ingredients):
+                line_ingredient = CookBook.readline().split('|')
+                load_cook_book = {
+                    'ingridient_name' : line_ingredient[0].strip().lower(),
+                    'quantity' : int(line_ingredient[1]),
+                    'measure' : line_ingredient[2].strip().lower()
+                }
+                ingredients.append(load_cook_book)
+                finish_cook_book.update({line_dish.strip().lower() : ingredients})
+                i += 1
+            CookBook.readline()
+    return  finish_cook_book
 
 
 create_shop_list()
